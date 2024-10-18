b388d9ff388fead9daee022f037828c0dc610355
naeemmca <85763202+naeemmca@users.noreply.github.com>
2024-10-18 10:40:26 +0530

    Create instance.tf

---
+++ instance.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "ap-southeast-1"  # Change to your preferred region
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-047126e50991d067b"  # Replace with a valid AMI ID for your region
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "naeim-instance"
+  }
+}
