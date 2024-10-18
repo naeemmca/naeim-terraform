469726a31a59cee550112058c11c3187de68a3ad
naeemmca <85763202+naeemmca@users.noreply.github.com>
2024-10-18 12:51:41 +0530

    Update key.tf

--- key.tf
+++ key.tf
@@ -6,12 +6,12 @@ resource "tls_private_key" "two-tier-key-pair" {
 
 #Storing the Public key in AWS
 resource "aws_key_pair" "two-tier-public-key" {
-  key_name   = "mehar-two-tier-public-key"
+  key_name   = "naeim-two-tier-public-key"
   public_key = tls_private_key.two-tier-key-pair.public_key_openssh #Passing the Public Key 
 }
 
 resource "aws_s3_object" "private_key_in_bucket" {
-  bucket = "mehar-two-tier-architecture-with-terraform"
+  bucket = "naeim-two-tier-architecture-with-terraform"
   depends_on = [ aws_s3_bucket.two-tier-architecture-with-terraform ]
   key    = "/two-tier-architecture-with-terraform/two-tier-key-pair.pem"
   content = tls_private_key.two-tier-key-pair.private_key_pem
